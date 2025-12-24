9073a1fd2ab78a015c41fe2a069c5c0728e79af2
sagar <kumarsagar792@gmail.com>
2025-12-24 17:01:41 +0530

    first commit

---
+++ src/main/java/com/github/commonds/CommondsGithubApplication.java
@@ -0,0 +1,13 @@
+package com.github.commonds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommondsGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommondsGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/commonds/CommondsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.commonds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommondsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
